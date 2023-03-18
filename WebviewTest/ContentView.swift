fb9299d15093133bf0c8ad839606d49c8b5880c8
Nitin Prakash <nitin@upswing.one>
2023-03-18 16:05:36 +0530

    Initial Commit

---
+++ WebviewTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebviewTest
+//
+//  Created by Nitin Prakash on 18/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebviewTest/WebviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewTestApp.swift
+//  WebviewTest
+//
+//  Created by Nitin Prakash on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WebviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
